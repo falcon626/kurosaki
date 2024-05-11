4bee39903ee1e9b2172c4e9b00fafe7a6ce0f205
falcon626 <626hayato@gmail.com>
2024-05-11 12:07:18 +0900

    シーンマネージャ
    整備後

---
+++ 002_シーンマネージャ(整備済)/Src/Application/Scene/SceneManager.cpp
@@ -0,0 +1,54 @@
+﻿#include "SceneManager.h"
+#include "TitleScene/TitleScene.h"
+#include "GameScene/GameScene.h"
+#include "BaseScene/BaseScene.h"
+
+
+void SceneManager::PreUpdate()
+{
+	// Scene切替
+	if (m_currentSceneType != m_nextSceneType)
+	{
+		ChangeScene(m_nextSceneType);
+	}
+}
+
+void SceneManager::Update()
+{
+	m_currentScene->Update();
+}
+
+void SceneManager::Draw()
+{
+	m_currentScene->Draw();
+}
+
+void SceneManager::Init()
+{
+	m_currentScene = std::make_shared<TitleScene>();
+}
+
+void SceneManager::Release()
+{
+}
+
+void SceneManager::ChangeScene(SceneType _sceneType)
+{
+	// 次のシーンを作成し、現在のシーンにする
+	switch (_sceneType)
+	{
+	case SceneManager::Title:
+		m_currentScene = std::make_shared<TitleScene>();
+		break;
+	case SceneManager::Game:
+		m_currentScene = std::make_shared<GameScene>();
+		break;
+	case SceneManager::Result:
+		break;
+	default:
+		break;
+	}
+
+	// シーン管理を更新
+	m_currentSceneType = _sceneType;
+}
---
+++ 002_シーンマネージャ(整備済)/Src/Application/Scene/SceneManager.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+// シーンマネージャは実行時には実体化している
+// #SceneManager.hをインクルードすればどこからでもシーン切替可能!!
+
+class BaseScene; // 前方宣言
+
+class SceneManager
+{
+public:
+	enum SceneType
+	{
+		Title,
+		Game,
+		Result
+	};
+
+	void PreUpdate();
+	void Update();
+
+	void Draw();
+
+	// 次のシーンを予約するための関数
+	void SetNextScene(SceneType _nextScene) { m_nextSceneType = _nextScene; }
+	
+	SceneType GetCurrentSceneType() { return m_currentSceneType; }
+
+private:
+	void Init();
+	void Release();
+
+	void ChangeScene(SceneType _sceneType);
+
+	std::shared_ptr<BaseScene> m_currentScene;
+	
+	SceneType m_currentSceneType = SceneType::Title;
+
+	SceneType m_nextSceneType = m_currentSceneType;
+
+// シングルトンパターン
+// そのクラスのインスタンスが1つしか生成されないことを保証するデザインパターン
+// あまり多用しない！！
+private:
+	SceneManager() { Init(); }
+	~SceneManager() { Release(); }
+public:
+	static auto& GetInstance(){ static SceneManager Instance; return Instance; }
+};
